bc68158f66d52f8a5044b8edcafd9dd2e22cec04
megaraph <47447126+megaraph@users.noreply.github.com>
2022-07-02 01:32:33 +0800

    build: change program structure to OOP

--- time_util.py
+++ time_util.py
@@ -1,123 +1,202 @@
+from typing import Dict
 import datetime
 
-MERIDIEMS = ["am", "pm"]
 
-# TODO: add type hints
-# TODO: method strategy: one for start stop time
-# one for video length
-def get_time_by_start_stop(type: str = "start"):
-    if type not in ["start", "stop"]:
-        print("Provide valid type: start or stop")
-        return None
+class Time:
+    _MERIDIEMS = ["am", "pm"]
+    _TIME_POSITIONS = ["start", "stop"]
 
-    time = input(f"{type.capitalize()} time: ") or None
-    meridiem = None
+    def __init__(self, time_pos: str):
+        self.time_pos = time_pos
+        self._inputted_time = self._get_time_from_input()
+        self._time = self.time
 
-    if type == "stop" and time is None:
-        print("Stop time must not be none")
-        exit()
+    # TODO: method strategy: one for start stop time
+    # one for video length
+    def _get_time_from_input(self) -> Dict[str, str]:
+        if self.time_pos not in self._TIME_POSITIONS:
+            print("Provide valid type: start or stop")
+            return None
 
-    if time is not None:
-        time = parse_time_from_string(time)
-        validate_time(time, type=type)
+        input_time = input(f"{self.time_pos.capitalize()} time: ") or None
 
-    return time, meridiem
+        if self.time_pos == "stop" and input_time is None:
+            print("Stop time must not be none")
+            exit()
+
+        if input_time is not None:
+            input_time = self._parse_time_from_string(input_time)
+            self._validate_time(input_time, time_pos=self.time_pos)
+
+        return input_time
+
+    @staticmethod
+    def _parse_time_from_string(time: str) -> Dict[str, str]:
+        if time is None:
+            return None
+
+        # 23 : 59 pm -> 23:59pm
+        time = time.replace(" ", "")
+        # 23:59pm -> [23, 59pm]
+        time = time.split(":")
+
+        hours, minutes = time
+        meridiem = None
+
+        if len(hours) not in range(1, 3):
+            print("Invalid hours format given")
+            exit()
+
+        # if minutes is formatted like this due
+        # to split method: 59pm
+        if len(minutes) == 4:
+            meridiem = minutes[-2:].lower()
+            minutes = minutes[:2]
+
+        if len(minutes) != 4 and len(minutes) != 2:
+            print("Invalid minutes format given")
+            exit()
+
+        joined_time = ":".join([hours, minutes])
+        time = {
+            "hours": hours,
+            "minutes": minutes,
+            "meridiem": meridiem,
+            "str_time": joined_time,
+            "str_time_with_meridiem": f"{joined_time}{meridiem}" if meridiem else None,
+        }
+
+        return time
+
+    @property
+    def time(self) -> datetime.datetime:
+        input_time = self._inputted_time
+        now = datetime.datetime.now()
+
+        if input_time is None:
+            return now
+
+        hour = input_time.get("hours")
+        minute = input_time.get("minutes")
+        meridiem = input_time.get("meridiem")
+
+        today = datetime.datetime.today()
+        year = today.year
+        month = today.month
+        day = today.day
+
+        hour = int(hour) + 12 if meridiem == "pm" else int(hour)
+        hour = 0 if hour == 12 and meridiem == "am" else hour
+        minute = int(minute)
+
+        self._time = datetime.datetime(year, month, day, hour, minute)
+
+        if now > self._time:
+            self._time = self._time.replace(day=day + 1)
+
+        return self._time
+
+    def _validate_meridiem(self, meridiem: str):
+        if meridiem is None:
+            return None
+
+        if meridiem not in self._MERIDIEMS:
+            print("Invalid meridiem given")
+            exit()
+
+    def _validate_time(self, time: dict, time_pos: str = "start"):
+        # TODO: write tests
+        # TODO: write custom exceptions
+
+        if time_pos == "start" and time is None:
+            return None
+
+        hours = time.get("hours")
+        minutes = time.get("minutes")
+        meridiem = time.get("meridiem")
+
+        self._validate_meridiem(meridiem)
+
+        if int(hours) not in range(0, 24):
+            print("Invalid hours format given")
+            exit()
+
+        if int(minutes) not in range(0, 60):
+            print("Invalid minutes format given")
+            exit()
+
+        return True
+
+    def __str__(self):
+        return f"{self.time_pos} | {self.time}"
 
 
-def parse_time_from_string(time: str):
-    # 23 : 59 pm -> 23:59pm
-    time = time.replace(" ", "")
-    # 23:59pm -> [23, 59pm]
-    time = time.split(":")
-
-    hours, minutes = time
-    meridiem = None
-
-    if len(hours) not in range(1, 3):
-        print("Invalid hours format given")
-        exit()
-
-    # if minutes is formatted like this due
-    # to split method: 59pm
-    if len(minutes) == 4:
-        meridiem = minutes[-2:].lower()
-        minutes = minutes[:2]
-
-    if len(minutes) != 4 and len(minutes) != 2:
-        print("Invalid minutes format given")
-        exit()
-
-    joined_time = ":".join([hours, minutes])
-    time = {
-        "hours": hours,
-        "minutes": minutes,
-        "meridiem": meridiem,
-        "str_time": joined_time,
-        "str_time_with_meridiem": f"{joined_time}{meridiem}" if meridiem else None,
-    }
-
-    return time
+class RecordingStartTime(Time):
+    def __init__(self):
+        super().__init__("start")
 
 
-def validate_meridiem(meridiem: str):
-    if meridiem is None:
-        return None
-
-    if meridiem not in ["am", "pm"]:
-        print("Invalid meridiem given")
-        exit()
+class RecordingStopTime(Time):
+    def __init__(self):
+        super().__init__("stop")
 
 
-def validate_time(time: dict, type: str = "start"):
-    # TODO: write tests
-    # TODO: write custom exceptions
+class RecordViaLength:
+    def __init__(self):
+        self.start_time = datetime.datetime.now()
+        self._recording_length = self._get_recording_length()
+        self._stop_time = self.stop_time
 
-    if type == "start" and time is None:
-        return None, None
+    @property
+    def stop_time(self):
+        return self.start_time + self._time_at_video_length(*self._recording_length)
 
-    hours = time.get("hours")
-    minutes = time.get("minutes")
-    meridiem = time.get("meridiem")
+    def _get_recording_length(self) -> str:
+        print(
+            "\nFormat: [hours]:[minutes]:[seconds]:[milliseconds] (Optional: milliseconds)"
+        )
+        print("Example: 01:42:30:00 <- (1 hour 42 minutes 30 seconds)")
+        recording_length = input("Recording length: ") or None
+        recording_length = self._parse_recording_length(recording_length)
 
-    validate_meridiem(meridiem)
+        return recording_length
 
-    if int(hours) not in range(0, 24):
-        print("Invalid hours format given")
-        exit()
+    @staticmethod
+    def _parse_recording_length(recording_length: str) -> Dict[str, int]:
+        if recording_length is None:
+            return (0, 0, 0, 0)
 
-    if int(minutes) not in range(0, 60):
-        print("Invalid minutes format given")
-        exit()
+        if len(recording_length) not in range(8, 12):
+            print(
+                "Invalid Format | Format: [hours]:[minutes]:[seconds]:[milliseconds] (Optional: milliseconds)"
+            )
 
-    return True
+        hrs, mins, secs, *ms = map(int, recording_length.split(":"))
 
+        if len(ms) > 0:
+            ms = ms[0]
+        else:
+            ms = 0
 
-def convert_date_time(time: dict):
-    # TODO: implement code if time is none and type is start (means get current time)
-    hour = time.get("hours")
-    minute = time.get("minutes")
-    meridiem = time.get("meridiem")
+        return (hrs, mins, secs, ms)
 
-    today = datetime.datetime.today()
-    year = today.year
-    month = today.month
-    day = today.day
+    @staticmethod
+    def _time_at_video_length(
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+    ):
+        return datetime.timedelta(
+            hours=hours,
+            minutes=minutes,
+            seconds=seconds,
+            milliseconds=milliseconds,
+        )
 
-    hour = int(hour) + 12 if meridiem == "pm" else int(hour)
-    hour = 0 if hour == 12 and meridiem == "am" else hour
-    minute = int(minute)
-
-    now = datetime.datetime.now()
-    converted = datetime.datetime(year, month, day, hour, minute)
-
-    if now > converted:
-        converted = converted.replace(day=day + 1)
-
-    return converted
+    def __str__(self):
+        return f"Start: {self.start_time} | Stop: {self.stop_time}"
 
 
 def get_current_time():
     return datetime.datetime.now()
-
-
-print(get_time_by_start_stop("start"))
